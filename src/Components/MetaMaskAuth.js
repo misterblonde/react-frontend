9b61001e4af1e39033dacba14b8ab55744f55b02
Julia <pythonoverjulia@gmail.com>
2022-09-09 03:00:15 +0100

    metamask injected connector bit commented

--- src/Components/MetaMaskAuth.js
+++ src/Components/MetaMaskAuth.js
@@ -1,75 +1,75 @@
-import React, { useEffect, useState } from "react";
-import styles from "./MetaMaskAuth.css";
-import Button from "@mui/material/Button";
-import "../index.css";
-function isMobileDevice() {
-  return "ontouchstart" in window || "onmsgesturechange" in window;
-}
+// import React, { useEffect, useState } from "react";
+// import styles from "./MetaMaskAuth.css";
+// import Button from "@mui/material/Button";
+// import "../index.css";
+// function isMobileDevice() {
+//   return "ontouchstart" in window || "onmsgesturechange" in window;
+// }
 
-async function connect(onConnected) {
-  if (!window.ethereum) {
-    alert("Get MetaMask!");
-    return;
-  }
+// async function connect(onConnected) {
+//   if (!window.ethereum) {
+//     alert("Get MetaMask!");
+//     return;
+//   }
 
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
+//   const accounts = await window.ethereum.request({
+//     method: "eth_requestAccounts",
+//   });
 
-  onConnected(accounts[0]);
-}
+//   onConnected(accounts[0]);
+// }
 
-async function checkIfWalletIsConnected(onConnected) {
-  if (window.ethereum) {
-    const accounts = await window.ethereum.request({
-      method: "eth_accounts",
-    });
+// async function checkIfWalletIsConnected(onConnected) {
+//   if (window.ethereum) {
+//     const accounts = await window.ethereum.request({
+//       method: "eth_accounts",
+//     });
 
-    if (accounts.length > 0) {
-      const account = accounts[0];
-      onConnected(account);
-      return;
-    }
+//     if (accounts.length > 0) {
+//       const account = accounts[0];
+//       onConnected(account);
+//       return;
+//     }
 
-    if (isMobileDevice()) {
-      await connect(onConnected);
-    }
-  }
-}
+//     if (isMobileDevice()) {
+//       await connect(onConnected);
+//     }
+//   }
+// }
 
-export default function MetaMaskAuth({ onAddressChanged }) {
-  const [userAddress, setUserAddress] = useState("");
+// export default function MetaMaskAuth({ onAddressChanged }) {
+//   const [userAddress, setUserAddress] = useState("");
 
-  useEffect(() => {
-    checkIfWalletIsConnected(setUserAddress);
-  }, []);
+//   useEffect(() => {
+//     checkIfWalletIsConnected(setUserAddress);
+//   }, []);
 
-  useEffect(() => {
-    onAddressChanged(userAddress);
-  }, [userAddress]);
+//   useEffect(() => {
+//     onAddressChanged(userAddress);
+//   }, [userAddress]);
 
-  return userAddress ? (
-    <div>
-      Connected with <Address userAddress={userAddress} />
-    </div>
-  ) : (
-    <Connect setUserAddress={setUserAddress} />
-  );
-}
+//   return userAddress ? (
+//     <div>
+//       Connected with <Address userAddress={userAddress} />
+//     </div>
+//   ) : (
+//     <Connect setUserAddress={setUserAddress} />
+//   );
+// }
 
-function Connect({ setUserAddress }) {
-  return (
-    <Button className={styles.Button} onClick={() => connect(setUserAddress)}>
-      Connect to MetaMask
-    </Button>
-  );
-}
+// function Connect({ setUserAddress }) {
+//   return (
+//     <Button className={styles.Button} onClick={() => connect(setUserAddress)}>
+//       Connect to MetaMask
+//     </Button>
+//   );
+// }
 
-function Address({ userAddress }) {
-  return (
-    <span className={styles.address}>
-      {userAddress.substring(0, 5)}…
-      {userAddress.substring(userAddress.length - 4)}
-    </span>
-  );
-}
+// function Address({ userAddress }) {
+//   return (
+//     <span className={styles.address}>
+//       {userAddress.substring(0, 5)}…
+//       {userAddress.substring(userAddress.length - 4)}
+//     </span>
+//   );
+// }
